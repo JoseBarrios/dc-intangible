574119722f29db16840d0df170e7e11c0c6d0a91
Jose Barrios <jose@cognilab.com>
2017-06-26 08:40:11 -0700

    Added test case

--- tests/test.js
+++ tests/test.js
@@ -2,3 +2,10 @@
 
 var assert = require('assert');
 var Intangible = require('../index.js');
+
+describe('Intangible.isNumber', function() {
+  it('should inheret Thing static method isNumber', function() {
+    assert.equal(Intangible.isNumber(1), true);
+    assert.equal(Intangible.isNumber('x'), false);
+  });
+});
